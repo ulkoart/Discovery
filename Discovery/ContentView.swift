164621963410ff127ef30931e1a876e2eb7f16d1
Artem Ulko <ulkoart@gmail.com>
2021-08-23 15:16:18 +0300

    update "Popular destinations"

--- Discovery/ContentView.swift
+++ Discovery/ContentView.swift
@@ -20,8 +20,18 @@ struct ContentView: View {
     }
 }
 
+struct Destination: Hashable {
+    let name, country, imageName: String
+}
 
 struct PopularDestinationsView: View {
+    
+    let destinations: [Destination] = [
+        .init(name: "Paris", country: "France", imageName: "eiffel_tower"),
+        .init(name: "Tokyo", country: "Japan", imageName: "japan"),
+        .init(name: "New York", country: "US", imageName: "new_york"),
+    ]
+    
     var body: some View {
         HStack {
             Text("Popular destinations")
@@ -34,10 +44,26 @@ struct PopularDestinationsView: View {
         
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 8) {
-                ForEach(0..<5, id: \.self) { num in
-                    Spacer()
-                        .frame(width: 125, height: 150, alignment: .center)
-                        .background(Color.gray)
+                ForEach(destinations, id: \.self) { destination in
+                    VStack(alignment: .leading, spacing: 0) {
+                        Image(destination.imageName)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 125, height: 125, alignment: .center)
+                            .cornerRadius(4)
+                            .padding(.horizontal, 6)
+                            .padding(.vertical, 6)
+ 
+                        Text(destination.name)
+                            .font(.system(size: 12, weight: .semibold))
+                            .padding(.horizontal, 12)
+                        Text(destination.country)
+                            .font(.system(size: 12, weight: .semibold))
+                            .padding(.horizontal, 12)
+                            .padding(.bottom, 8)
+                    }
+                        .frame(width: 125)
+                        .background(Color(.init(white: 0.9, alpha: 1)))
                         .cornerRadius(5)
                         .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
                         .padding(.bottom)
@@ -115,8 +141,8 @@ struct DiscoverCategoriesView: View {
         .init(name: "Art", imageName: "paintpalette.fill"),
         .init(name: "Sport", imageName: "sportscourt.fill"),
         .init(name: "Live Events", imageName: "music.mic"),
-        .init(name: "Food", imageName: "music.mic"),
-        .init(name: "History", imageName: "music.mic"),
+        .init(name: "Food", imageName: "eye.fill"),
+        .init(name: "History", imageName: "book.fill"),
     ]
     
     var body: some View {
