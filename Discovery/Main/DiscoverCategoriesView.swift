ac31dcc03d05454d41d7292182663bc3896164d3
Artem Ulko <ulkoart@gmail.com>
2021-08-24 16:19:37 +0300

    init CategoryDetailsView

--- Discovery/Main/DiscoverCategoriesView.swift
+++ Discovery/Main/DiscoverCategoriesView.swift
@@ -21,30 +21,60 @@ struct DiscoverCategoriesView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .top, spacing:14) {
                 ForEach(categories, id: \.self) { category in
-                    VStack(spacing: 8) {
-                        Image(systemName: category.imageName)
-                            .font(.system(size: 20))
-                            .foregroundColor(Color(#colorLiteral(red: 0.9999931455, green: 0.5843424201, blue: 0.2549082935, alpha: 1)))
-                            .frame(width: 64, height: 64)
-                            .background(Color.white)
-                            .cornerRadius(64)
-                        Text(category.name)
-                            .font(.system(size: 12, weight: .semibold))
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.white)
-                    }.frame(width: 68)
+                    NavigationLink(
+                        destination: CategoryDetailsView(),
+                        label: {
+                            VStack(spacing: 8) {
+                                Image(systemName: category.imageName)
+                                    .font(.system(size: 20))
+                                    .foregroundColor(Color(#colorLiteral(red: 0.9999931455, green: 0.5843424201, blue: 0.2549082935, alpha: 1)))
+                                    .frame(width: 64, height: 64)
+                                    .background(Color.white)
+                                    .cornerRadius(64)
+                                Text(category.name)
+                                    .font(.system(size: 12, weight: .semibold))
+                                    .multilineTextAlignment(.center)
+                                    .foregroundColor(.white)
+                            }.frame(width: 68)
+                        })
                 }
             }.padding(.horizontal)
         }
     }
 }
 
+struct CategoryDetailsView: View {
+    var body: some View {
+        ScrollView {
+            
+            ForEach(0..<5, id: \.self) { num in
+                VStack(alignment: .leading, spacing: 0) {
+                    Image("art1")
+                        .resizable()
+                        .scaledToFill()
+                    Text("Demo")
+                        .font(.system(size: 12, weight: .semibold))
+                        .padding()
+                }.asTile()
+                .padding()
+            }
+
+        }.navigationBarTitle("Categoty", displayMode: .inline)
+    }
+}
+
 struct DiscoverCategoriesView_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack {
-            Color.orange
-            DiscoverCategoriesView()
+        NavigationView {
+            CategoryDetailsView()
         }
+        
+        MainView()
+        
+//        ZStack {
+//            Color.orange
+//            DiscoverCategoriesView()
+//        }
         
     }
 }
