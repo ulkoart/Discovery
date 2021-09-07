2d1c7d314d70bb8c85960312f7240a0ae2800db2
Artem Ulko <ulkoart@gmail.com>
2021-09-07 15:06:40 +0300

    LazyVGrid - fix

--- Discovery/Main/Restaurant/RestaurantPhotosView.swift
+++ Discovery/Main/Restaurant/RestaurantPhotosView.swift
@@ -34,7 +34,7 @@ struct RestaurantPhotosView: View {
         GeometryReader { proxy in
             ScrollView {
                 LazyVGrid(columns: [
-                    GridItem(.adaptive(minimum: proxy.size.width/3 - 12, maximum: 300), spacing: 2)
+                    GridItem(.adaptive(minimum: proxy.size.width / 3 - 4, maximum: 300), spacing: 2)
                 ], spacing: 4, content: {
                     ForEach(photoUrlStrings, id: \.self) { urlString in
                         KFImage(URL(string: urlString))
@@ -43,7 +43,7 @@ struct RestaurantPhotosView: View {
                             .frame(width: proxy.size.width/3 - 3, height: proxy.size.width/3 - 3)
                             .clipped()
                     }
-                }).padding(.horizontal)
+                }).padding(.horizontal, 2)
             }
         }
         .navigationBarTitle("All Photos", displayMode: .inline)
